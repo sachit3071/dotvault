24cf44bc8d923ac80ec40336b51bea4838739294
Sachit Gupta <sachit2071@gmail.com>
2026-04-20 20:43:47 +0530

    feat/github aliases and git stash and git add

--- git-aliases.sh
+++ git-aliases.sh
@@ -1,6 +1,9 @@
 # =========================
 # Git Aliases
 # =========================
+alias ga='git add'
+alias gaa='git add .'
+alias gb='git branch'
 alias gcm='git commit -m'
 alias gco='git checkout'
 alias push='git push'
@@ -10,7 +13,15 @@ alias grs='git restore .'
 alias gss='git status --short'
 alias grv='git revert -m 1'
 alias gst='git stash'
+alias gstl='git stash list'
 alias gstp='git stash pop'
+
+# =========================
+# GitHub CLI Aliases
+# =========================
+alias ghprcf='gh pr create --fill'
+alias ghprl='gh pr list'
+alias ghprv='gh pr view'
 
 # =========================
 # Python / Dev Aliases
