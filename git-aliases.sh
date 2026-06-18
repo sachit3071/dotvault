9201e514669e63dbe344b2eb6cff628efd50f62e
sachit3071 <sachit3071@gmail.com>
2026-06-18 22:44:53 +0530

    update: stash staged changes only and revert by commit hash

--- git-aliases.sh
+++ git-aliases.sh
@@ -12,10 +12,13 @@ alias gcob='git checkout -b'
 alias grs='git restore .'
 alias gpum='git push -u origin HEAD'
 alias gss='git status --short'
-alias grv='git revert -m 1'
+alias grv='git revert'
+alias grvm='git revert -m 1'
 alias gst='git stash'
 alias gstl='git stash list'
 alias gstp='git stash pop'
+alias gstsp='git stash push --staged'
+alias gcl='git clone'
 
 # =========================
 # GitHub CLI Aliases
